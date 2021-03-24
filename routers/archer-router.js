4d7e6d5c0e4a6e3d349816e70e93feed81af94ba
Amanda Nelson <amanda.gaylord@gmail.com>
2021-03-24 19:01:47 -0400

    setting up

---
+++ routers/archer-router.js
@@ -0,0 +1,7 @@
+//POST and DELETE MVP
+const { router } = require("../server");
+const db = require("./archer-model");
+
+router.post("/character", (req, res, next) => {});
+router.delete("/character/:id", (req, res, next) => {});
+module.exports = router;
